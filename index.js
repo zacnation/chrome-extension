f7b7032aacd5e2addabd2dc351a1a20829b99636
zac nation <zac.nation@icloud.com>
2023-08-25 18:52:03 +0100

    Check local storage before rendering

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const ulEl = document.getElementById('ul-el');
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
