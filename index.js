bffda51e46bd5b35a3a74751ebe9a382e87cb69f
zac nation <zac.nation@icloud.com>
2023-08-25 11:36:37 +0100

    Add ul + render li elements

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
 
-let myLeads = [];
+let myLeads = ['one', 'two', 'three'];
 
 inputBtn.addEventListener('click', () => {
   myLeads.push(inputEl.value);
 });
+
+for (let lead of myLeads) {
+  ulEl.innerHTML += `<li>${lead}</li>`;
+}
