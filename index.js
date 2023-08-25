de166d11f0a30932c9007aeaf6e025d7ce7dd530
zac nation <zac.nation@icloud.com>
2023-08-25 18:59:33 +0100

    Add delete button and functionality

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
+const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
@@ -14,6 +15,12 @@ inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
   inputEl.value = '';
   localStorage.setItem('myLeads', JSON.stringify(myLeads));
+  renderLeads();
+});
+
+deleteBtn.addEventListener('dblclick', function () {
+  localStorage.clear();
+  myLeads = [];
   renderLeads();
 });
 
